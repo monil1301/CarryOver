77cd8a43926ac082e1d4cc3ec556c23c0464671d
monil1301 <70565653+monil1301@users.noreply.github.com>
2026-03-05 17:40:59 +0530

    Initial Commit

---
+++ CarryOver/CarryOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarryOverApp.swift
+//  CarryOver
+//
+//  Created by Monil Shah on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CarryOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
